41d02a770dce7437e57de8d70d8b3ee4dcf3ca95
Neeraj Jaiswal <neerajj85@gmail.com>
2019-02-21 16:44:05 +0530

    implemented

--- src/twenty48/core.clj
+++ src/twenty48/core.clj
@@ -1,22 +1,54 @@
 (ns twenty48.core
   (:gen-class))
 
-(defn move-grid-right
+(def zeroes
+  (comp
+   (partial apply repeat)
+   reverse
+   (partial list 0)
+   (partial apply -)
+   (juxt first (comp count second))))
+
+(def pad-left
+  (comp
+   (partial apply concat)
+   (juxt second zeroes)))
+
+(def move-left
+  (comp
+   (partial map (partial apply +))
+   (partial mapcat (partial partition-all 2))
+   (partial partition-by identity)
+   (partial filter (comp not zero?))))
+
+(def move-left-with-pad
+  (comp
+   pad-left
+   (juxt count move-left)))
+
+(def move-right-with-pad
+  (comp reverse move-left-with-pad reverse))
+
+(def move-grid-right
   "Moves an entire grid to the right"
-  [grid]
-  grid)
+  (partial map move-right-with-pad))
 
-(defn move-grid-left
+(def move-grid-left
   "Moves an entire grid to the left"
-  [grid]
-  grid)
+  (partial map move-left-with-pad))
 
-(defn move-grid-down
+(def transpose (partial apply map list))
+
+(def move-grid-down
   "Moves an entire grid down"
-  [grid]
-  grid)
+  (comp
+   transpose
+   move-grid-right
+   transpose))
 
-(defn move-grid-up
+(def move-grid-up
   "Moves an entire grid up"
-  [grid]
-  grid)
+  (comp
+   transpose
+   move-grid-left
+   transpose))
